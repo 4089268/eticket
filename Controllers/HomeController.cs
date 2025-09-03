c4537a6a85d48b03c7d12a64a09d7428fca77d24
Salvador Rangel A <chava_r17@hotmail.com>
2025-09-03 00:36:03 -0600

    Updated dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,19 +2,18 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using eticket.Models;
 using Microsoft.AspNetCore.Authorization;
+using eticket.Core.Interfaces;
 
 namespace eticket.Controllers;
 
 [Authorize]
-public class HomeController : Controller
+[Route("/{Controller}")]
+public class HomeController(ILogger<HomeController> l, IResumeService rs) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<HomeController> logger = l;
+    private readonly IResumeService resumeService = rs;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
+    [Route("/")]
     public IActionResult Index()
     {
         return View();
@@ -30,4 +29,41 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+
+    #region PartialViews
+    [HttpGet("partial-view/status-resume")]
+    public IActionResult ResumenEstatusPartialView()
+    {
+        try
+        {
+            var resumen = this.resumeService.ObtenerResumenPorEstatus();
+            return PartialView("~/Views/Home/Partials/ResumenEstatus.cshtml", resumen);
+        }
+        catch (Exception err)
+        {
+            this.logger.LogError(err, "Error al obtener el resumen por estatus");
+            ViewData["ErrorTitle"] = "Error al obtener el resumen por estatus";
+            ViewData["ErrorMessage"] = err.Message;
+            return PartialView("~/Views/Shared/_ErrorAlert.cshtml");
+        }
+    }
+
+    [HttpGet("partial-view/tipo-reporte-resume")]
+    public IActionResult ResumenTipoReportePartialView()
+    {
+        try
+        {
+            var resumen = this.resumeService.ObtenerResumenPorTipoReporte();
+            return PartialView("~/Views/Home/Partials/ResumenTipo.cshtml", resumen);
+        }
+        catch (Exception err)
+        {
+            this.logger.LogError(err, "Error al obtener el resumen por estatus");
+            ViewData["ErrorTitle"] = "Error al obtener el resumen por estatus";
+            ViewData["ErrorMessage"] = err.Message;
+            return PartialView("~/Views/Shared/_ErrorAlert.cshtml");
+        }
+    }
+    #endregion
 }
